9ad7e6efcb536989d0e1b0c21026e0c77168aa9a
pl1nk <adeellq8@gmail.com>
2023-09-11 14:00:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
-import webbrowser
-webbrowser.open 
+from bs4 import BeautifulSoup
+import requests
+import re
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0'
+}
+
+product = input("What do you want? ")
+
+xcite_url = f'https://www.xcite.com/search?q={product}&toggle%5BinStock%5D=true'
+
+response = requests.get(xcite_url, headers=headers)
+
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+product_divs = soup.find_all('div', class_='ProductTile_wrapperXPYR3')
+
+if product_divs:
+    for product_div in product_divs:
+        title = product_div.find('p', class_='ProductTile_productNameR9tA5')
+        price = product_div.find('span', class_='text-2xl text-functional-red-800 block mb-2')
+        if title and price:
+            title_text = title.text.strip()
+            price_text = price.text.strip()
+            print("Product Title:", title_text)
+            print("Product Price:", price_text)
+        else:
+            print("Product title or price not found for one of the products.")
+    else:
+        print("No products found on the page.")
+else:
+    print("Failed to retrieve the page. Status code:", response.status_code)
